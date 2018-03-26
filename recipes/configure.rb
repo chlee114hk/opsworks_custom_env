eb82840acf70c36c6863010e94ea376bef7d5eaa
aekazitt <aekazitt@gmail.com>
2018-03-26 14:31:23 +0800

    Update ownership

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -1,7 +1,12 @@
 node[:deploy].each do |app, deploy|
-  puts "App: #{app}"
-  puts "Deploy: #{deploy}"
+  directory "var/www/#{app}/config" do
+    owner "ubuntu"
+    group "www-data"
+    mode 0774
+    recursive true
+    action :create
+  end
   file File.join('var', 'www', "#{app}", 'config', 'app_data.yml') do
-    content YAML.dump(deploy[:custom_env].to_hash)
+    content YAML.dump({ :production => deploy[:custom_env] }.to_hash)
   end
 end
