cb99d69b395fa81e7ac7aeb4828efc27debecf57
aekazitt <aekazitt@gmail.com>
2018-03-26 15:38:14 +0800

    Removed to_json

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -7,6 +7,6 @@ node[:deploy].each do |app, deploy|
     action :create
   end
   file File.join('var', 'www', "#{app}", 'config', 'app_data.yml') do
-    content YAML.dump({ :production => deploy[:custom_env] }.to_json)
+    content YAML.dump({ :production => deploy[:custom_env] })
   end
 end
