a0a34edf5656ec8729f78d1d8fb1567e27851e88
aekazitt <aekazitt@gmail.com>
2018-03-27 11:13:53 +0800

    Flush out previous .env on configure

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -1,11 +1,17 @@
 node[:deploy].each do |app, deploy|
-  directory "var/www/#{app}/config" do
+  directory "var/www/#{app}" do
     owner "ubuntu"
     group "www-data"
     mode 0774
     recursive true
     action :create
   end
+  
+  file File.join('var', 'www', "#{app}", '.env') do
+    only_if { File.exist?(File.join('var', 'www', "#{app}", '.env')) }
+    action :touch
+  end
+  
   file File.join('var', 'www', "#{app}", '.env') do
     dotenv = ''
     deploy[:custom_env].each {|key, value| dotenv << "%s=%s\n" % [key, value]}
